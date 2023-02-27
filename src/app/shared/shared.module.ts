1cb6b07bf5e7531a75b32bc2caf51068b967513c
ivan retamozo <retamozoi445@gmail.com>
2023-02-27 20:53:18 -0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClarityModule } from '@clr/angular';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule, ClarityModule],
+})
+export class SharedModule {}
